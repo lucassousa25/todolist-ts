3908070ffd01bf65a05380a83f8520eccdcbabe0
lucassousa25 <lucassousao2510@gmail.com>
2022-05-31 22:06:29 -0300

    Adicionando Types e State List

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
+import { useState } from 'react';
 import * as S from './App.styles';
+import { Item } from './types/Item';
 
 const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Estudar ReactJS p/ 30 minutos', done: false },
+    { id: 2, name: 'Estudar TypeScript p/ 15 minutos', done: false }
+  ]);
+
   return (
     <S.Container>
       <S.Area>
