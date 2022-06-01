eeba216fedf5ebd1cf03e9faf01d9738f9bba9e7
lucassousa25 <lucassousao2510@gmail.com>
2022-06-01 20:48:46 -0300

    Setando line-through quando marcado

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App = () => {
 
   const [list, setList] = useState<Item[]>([
     { id: 1, name: 'Estudar ReactJS p/ 30 minutos', done: false },
-    { id: 2, name: 'Estudar TypeScript p/ 15 minutos', done: false }
+    { id: 2, name: 'Estudar TypeScript p/ 15 minutos', done: true }
   ]);
 
   return (
--- src/components/ListItem/index.tsx
+++ src/components/ListItem/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import * as S from './styles';
 import { Item } from '../../types/Item';
 
@@ -6,11 +8,15 @@ type Props = {
 };
 
 export const ListItem = ({ item }: Props) => {
-  
+  const [isChecked, setIsChecked] = useState(item.done);
 
   return (
-    <S.Container>
-      <input type="checkbox" />
+    <S.Container done={isChecked}>
+      <input
+        type="checkbox"
+        checked={isChecked}
+        onChange={e => setIsChecked(e.target.checked)}
+      />
       <label>{item.name}</label>
     </S.Container>
   )
