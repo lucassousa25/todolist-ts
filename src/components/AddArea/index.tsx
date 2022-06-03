b3179b44730a00e29b876161d08ef866d5fc7db8
lucassousa25 <lucassousao2510@gmail.com>
2022-06-02 21:13:02 -0300

    Adicionando função Enter em componente AddArea

--- src/components/AddArea/index.tsx
+++ src/components/AddArea/index.tsx
@@ -1,10 +1,30 @@
+import { useState, KeyboardEvent } from 'react';
 import * as S from './styles';
 
-export const AddArea = () => {
+type Props = {
+  onEnter: (taskName: string) => void
+};
+
+export const AddArea = ({ onEnter }: Props) => {
+  const [inputText, setInputText] = useState('');
+
+  const handleKeyUp = (e: KeyboardEvent) => {
+    if((e.code === 'Enter' || e.code === 'NumpadEnter') && inputText !== ''){
+      onEnter(inputText);
+      setInputText('');
+    }
+  }
+
   return (
     <S.Container>
       <div className='image'>➕</div>
-      <input type="text" placeholder='Adicione uma tarefa...' />
+      <input 
+        type="text"
+        placeholder='Adicione uma tarefa...'
+        value={inputText}
+        onChange={e => setInputText(e.target.value)}
+        onKeyUp={handleKeyUp}
+      />
     </S.Container>
   )
 }
